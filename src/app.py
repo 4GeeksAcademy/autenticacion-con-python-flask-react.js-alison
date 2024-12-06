630412a1312fffb79cd8118f3d4072d8542bfd8b
AlisonCastroM <alisoncastro7616@gmail.com>
2024-12-06 17:02:29 +0000

    finalizado

--- src/app.py
+++ src/app.py
@@ -10,14 +10,25 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
 
 # from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+CORS(app, origins=["https://verbose-space-doodle-7vrxv7q4qrw5fxx9r-3001.app.github.dev/"])
+
+app.config["JWT_SECRET_KEY"] = "ThisIsCool"  # Change this!
+jwt = JWTManager(app)
+
+
+
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -64,7 +75,6 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
-
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
